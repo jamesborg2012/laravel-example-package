1e1cee493159e45b36accfad70e31b898a3031f6
jamesborg2012 <jamesborg2012@gmail.com>
2023-11-19 20:59:47 +0100

    Created a migration for Persons table with Model and Controller
    Signed-off-by: jamesborg2012 <jamesborg2012@gmail.com>

---
+++ src/Http/Controllers/PersonController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jamesborg2012\LaravelExamplePackage\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PersonController extends Controller
+{
+    //
+}
